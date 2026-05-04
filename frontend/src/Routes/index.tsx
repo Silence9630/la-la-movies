67d7cf7179af6b4204da5164605ca3e793565b3e
Chris-mucyo <chrisxeventy6@gmail.com>
2026-05-04 07:53:44 -1100

    Auth done

--- frontend/src/Routes/index.tsx
+++ frontend/src/Routes/index.tsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { lazy, Suspense } from 'react';
 
 const LoginPage  =lazy(() => import('../features/Auth/login'))
+const SignupPage =lazy(() => import('../features/Auth/signup'))
+const ForgotPassword =lazy(() => import('../features/Auth/forgotPassword'))
 
 export default function Router() {
     return (
         <BrowserRouter>
            <Suspense fallback={null}>
               <Routes>
-                <Route path="/Auth/login" element={<LoginPage />} />
+                <Route path="/Auth/signin" element={<LoginPage />} />
+                <Route path="/Auth/signup" element={<SignupPage />} />
+                <Route path="/Auth/forgot-password" element={<ForgotPassword />} />
               </Routes>
            </Suspense>
         </BrowserRouter>
